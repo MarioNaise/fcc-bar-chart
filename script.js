aa0ebca40b63a608fbdfc0fc039aec7355de4487
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-12-21 23:17:04 +0000

    fetch data

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-const dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
-
+(function(){
+  const dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+  
+  const getData = async () => {
+    const response = await fetch(dataUrl);
+    return await response.json();
+  }
+  
+  const renderData = async() => {
+    const dataObj = await getData();
+    const data = dataObj.data;
+    console.log(data);
+  }
+  
+  renderData();
+})()
