b7dac91486ac798aa8a3002371e8b8999a261fad
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-12-21 23:52:24 +0000

    adding svg and adding rects

--- script.js
+++ script.js
@@ -9,8 +9,24 @@
   }
   
   const renderData = async() => {
-    const data = await getData();
-    console.log(data);
+    const dataset = await getData();
+    console.log("dataset: ", dataset);
+    const w = 500;
+    const h = 100;
+
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+
+    svg.selectAll("rect")
+       .data(dataset)
+       .enter()
+       .append("rect")
+       .attr("x", (d, i) => i * 30)
+       .attr("y", (d, i) => h - d[1])
+       .attr("width", 25)
+       .attr("height", (d, i) => d[1])
   }
   
   renderData();
