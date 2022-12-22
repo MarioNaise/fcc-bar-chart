d51fcb7867a178e2a8378f95e4d90de7a94dc918
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-12-22 00:33:38 +0000

    adding title

--- script.js
+++ script.js
@@ -10,24 +10,25 @@
   
   const renderData = async() => {
     const dataset = await getData();
-    console.log("dataset: ", dataset);
-    const w = 500;
-    const h = 100;
-
-    const svg = d3.select("body")
-                  .append("svg")
-                  .attr("width", w)
-                  .attr("height", h);
-
+    const w = 1000;
+    const h = 500;
+    
+    const svg = d3.select("main")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+    
     svg.selectAll("rect")
-       .data(dataset)
-       .enter()
-       .append("rect")
-       .attr("x", (d, i) => i * 30)
-       .attr("y", (d, i) => h - d[1])
-       .attr("width", 25)
-       .attr("height", (d, i) => d[1])
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => i * 10)
+    .attr("y", (d, i) => h - d[1])
+    .attr("width", 25)
+    .attr("height", (d, i) => d[1])
+    
   }
+
   
   renderData();
 })()
