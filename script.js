7caaed29cb32e5672cd8c4ef69442d9bfb04bf1a
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-12-22 01:25:39 +0000

    adding xScale

--- script.js
+++ script.js
@@ -12,6 +12,17 @@
     const dataset = await getData();
     const w = 1000;
     const h = 500;
+    const padding = 30;
+
+    const xScale = d3.scaleLinear()
+    .domain([0, dataset.length])
+    .range([padding, w - padding]);
+
+    const yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset, (d) => d[1])])
+    .range([h - padding, padding]);
+
+    console.log(yScale(200))
     
     const svg = d3.select("main")
     .append("svg")
@@ -22,10 +33,10 @@
     .data(dataset)
     .enter()
     .append("rect")
-    .attr("x", (d, i) => i * 10)
-    .attr("y", (d, i) => h - d[1])
-    .attr("width", 25)
-    .attr("height", (d, i) => d[1])
+    .attr("x", (d, i) => xScale(i))
+    .attr("y", (d) => h - d[1] / 50)
+    .attr("width", ((w - (padding*2)) / dataset.length ) * 0.67)
+    .attr("height", (d) => d[1] / 50)
     
   }
 
